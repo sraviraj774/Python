de4bfaecd0839a0d2deb6863539d6fa0cd123c02
Ravi Raj Singh <113600694+sraviraj774@users.noreply.github.com>
2024-08-14 16:58:33 +0530

    Lecture 6

---
+++ Variable06.py
@@ -0,0 +1,51 @@
+print("Variable")
+
+# Variable is space or container in which we store value or data for further operations.
+
+# Ways to assign values to variable
+a = 10
+b = 5
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a**b)
+
+x = y = z = 10
+print(x,y,z)
+
+
+data = [1, 2, 3, 4]
+a, b, c, d = data
+
+print(data)
+print(c,d)
+
+# Taking input from the user
+
+s = int(input(" Enter first number"))
+t = int(input(" Enter second number"))
+
+print(s + t)
+print(s - t)
+print(s * t)
+print(s / t)
+print(s ** t)
+
+# Variable naming conventions
+# Never use space between name of the variable. Use underscore _ instead of space.
+# Use Capital Letters for representing name properly. For Example - MyVariableName.
+# Never use keywords as the name of variable.
+
+# Arithmatic operations with assignments
+n = 10
+print(n)
+n += 1
+print(n)
+n -= 1
+print(n)
+n *= 2
+print(n)
+n /= 2
+print(n)
+
